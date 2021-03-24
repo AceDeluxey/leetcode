a6f7f6d9d084d19edbda2a39ba92ca2d934cf86c
pdh <1154621892@qq.com>
2021-03-24 09:44:01 +0800

    class424 with some problems 2

--- src/class424.java
+++ src/class424.java
@@ -1,4 +1,5 @@
 //424. 替换后的最长重复字符
+//时间复杂度太高 没跑过
 public class class424 {
     public int characterReplacement(String s, int k) {
         int base=0,mid=0,top=0;
@@ -6,14 +7,20 @@ public class class424 {
         int cnt=0;
         int result=0;
         String temp=new String();
-        for(int i=0;i<=s.length()+1;i++){
-            if(result==s.length()){
+        for(int i=0;i<=s.length();i++){
+            if(result>=s.length()){
                 break;
+            }
+            if(i==s.length()  ){
+                if(result< s.length()-1){
+                    break;
+                }
             }
             int index=i%s.length();
             if(base==top || s.substring(index,index+1).equals(temp)  ){
                 if(base==top){
                     temp=s.substring(base,base+1);
+                    i=base;
                 }
 
                 top=(top+1)%s.length();
@@ -50,11 +57,11 @@ public class class424 {
                     } else{
                         cnt++;
                         top++;
-                        if(index==s.length()-1){
+
                             if(cnt>result){
                                 result=cnt;
                             }
-                        }
+
                         change_num++;
                     }
 
