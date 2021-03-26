771e546b6cbc3b60b83757e10a076d32fb3712b8
pdh <1154621892@qq.com>
2021-03-26 09:41:10 +0800

    class42 method2

--- src/class42.java
+++ src/class42.java
@@ -1,9 +1,35 @@
 //42. 接雨水
-//2021. 3.23 写了个死循环检验程序状态，但是这个死循环条件难以斟酌，数据较大时超时
+
 public class class42 {
     public int trap(int[] height) {
+        /**2021.3.25
+         * 思想和132范式那题类似，都是以中间为轴遍历，这样一边的最大值可以直接得出
+         * 时间15%,空间8%，还有很大优化空间，可以用栈 */
 
-        int base=0,vol=0;
+        int max_left=0;
+        int vol=0;
+        for(int i=0;i<height.length;i++){  //只循环右边即可
+            int max_right=0;
+            if(i!=height.length-1){
+                max_right=height[i+1];
+                for(int j=i+1;j<height.length;j++){
+                    if(height[j]>max_right){
+                        max_right = height[j];
+                    }
+                }
+            }
+            int sur=Math.min(max_left,max_right)-height[i];
+            if(sur>0){
+                vol+=sur;
+            }
+            if(height[i]>max_left){
+                max_left=height[i]; //一次遍历时就可以求出右边的最大值
+            }
+        }
+        return vol;
+
+        //2021. 3.23 以行为基本单位，写了个死循环检验程序状态，但是这个死循环条件难以斟酌，数据较大时超时
+        /**int base=0,vol=0;
         int cnt=9;
         if(height.length ==0||height.length ==1){
             vol =0;
@@ -32,6 +58,11 @@ public class class42 {
 
 
 
-        return vol;
+        return vol;*/
     }
 }
+//class42 test =new class42();
+//        int [] a={0,1,0,2,1,0,1,3,2,1,2,1};
+//        int []b={4,2,0,3,2,5};
+//        int []c={2,0,2};
+//        System.out.println(test.trap(b));
