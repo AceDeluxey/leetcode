59a806edfd9843422b7a850530e3bd4fc830c5df
pdh <1154621892@qq.com>
2021-04-04 12:42:38 +0800

    TreeNode convert:从数组到二叉树

--- src/DataStructure/TreeNode.java
+++ src/DataStructure/TreeNode.java
@@ -1,13 +1,55 @@
 package DataStructure;
-
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 public class TreeNode {
+    public int level=1;
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x) { val = x; }
+    public TreeNode(int x) {
+        this.val = x;
+        this.left=null;
+        this.right = null;
+    }
     public TreeNode(int val, TreeNode left, TreeNode right) {
          this.val = val;
           this.left = left;
           this.right = right;
       }
+    public  TreeNode convert(Integer[] array) {
+        int floor = 0, count = 0;
+        TreeNode[] treeNodes = new TreeNode[array.length];
+        while (array != null && count < array.length) {
+            int start = (int) Math.pow(2, floor) - 1;
+            int end = (int) Math.pow(2, floor + 1) - 1;
+            if (end > array.length) {
+                end = array.length;
+            }
+            for (int i = start; i < end; i++) {
+                if (array[i] != null) {
+                    treeNodes[i] = new TreeNode(array[i]);
+                } else {
+                    treeNodes[i] = null;
+                }
+                if (i > 0) {
+                    int parent = (i - 1) / 2;
+                    if (parent >= 0) {
+                        TreeNode pNode = treeNodes[parent];
+                        if (pNode != null) {
+                            if (i % 2 == 1) {
+                                pNode.left = treeNodes[i];
+                            } else {
+                                pNode.right = treeNodes[i];
+                            }
+                        }
+                    }
+                }
+                count++;
+            }
+            floor++;
+        }
+        return treeNodes[0];
+    }
 }
