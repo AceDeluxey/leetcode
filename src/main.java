3720158aa49bfb51abd95187f9876357b3e46694
pdh <1154621892@qq.com>
2021-05-03 18:15:18 +0800

    凑数

--- src/main.java
+++ src/main.java
@@ -1,9 +1,8 @@
-import DataStructure.ListNode;
-
 public class main {
     public static void main(String[] args) {
-
-
+        class403 test=new class403();
+        int []a={0,1,2,3,5,6,9};
+        System.out.println(test.canCross(a));
 
     }
 }
