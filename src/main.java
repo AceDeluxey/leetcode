5e57774c1aab79d6bcda1358e90b86e5d08c8405
pdh <1154621892@qq.com>
2021-05-25 21:51:01 +0800

    凑一下

--- src/main.java
+++ src/main.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import  DataStructure.TreeNode;
 public class main {
     public static void main(String[] args) {
-        class403 test=new class403();
-        int []a={0,1,2,3,5,6,9};
-        System.out.println(test.canCross(a));
+        int []a= {2,3,5};
+        int []b={1,2,4};
+        class1685 test = new class1685();
+        System.out.println(test.getSumAbsoluteDifferences(a));
+
+
+//        树的构造
+//        Integer [] a={1,2,3};
+//        TreeNode root1 =new TreeNode(a);
+//        Integer [] b={1,3,2};
+//        TreeNode root2 =new TreeNode(b);
+//        class872 test1 = new class872();
+//        System.out.println(test1.leafSimilar(root1,root2));
 
     }
 }
