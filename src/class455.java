5276709c7224fb044d4e7d88e053193a0bac6c9b
PDH <1154621892@qq.com>
2021-06-16 09:40:13 +0800

    455. 分发饼干 贪心

---
+++ src/class455.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/**
+ *  455. 分发饼干
+     假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+     对每个孩子 i，都有一个胃口值g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j]。如果 s[j]>= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+
+    思路：贪心，把食量和饼干排序，然后顺序用每个饼干去试孩子，如果不能满足某个孩子，则也不能满足其他孩子，直接j++，能满组就i++j++
+
+        8ms 90%
+ */
+public class class455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        for(  ; i<g.length && j<s.length; ){
+            if(s[j]>=g[i]){
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i;
+    }
+}
