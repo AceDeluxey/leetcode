6fa92c2dda78bb9691de7d8627df5935a152e092
PDH <1154621892@qq.com>
2021-06-25 10:31:01 +0800

    main

--- src/main.java
+++ src/main.java
@@ -1,9 +1,16 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.TreeSet;
 
 import  DataStructure.TreeNode;
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = "2019-01-03 10:59:27";
+        Date date = simpleDateFormat.parse(dateStr);
+        System.out.println(date);
         int []a={9,10,8,7};
         int []b={5,6,7,8};
         char c='中';
@@ -11,8 +18,13 @@ public class main {
         class455 test = new class455();
         class1510 test1 = new class1510();
 //        System.out.println(test1.winnerSquareGame(15));
-        class870 test2 = new class870();
-        System.out.println(test2.advantageCount(a,b));
+        class1600 test3 = new class1600("king");
+        test3.birth("king","andy");
+        test3.birth("king","bob");
+        test3.birth("andy","s");
+        System.out.println(test3.getInheritanceOrder());
+//        class870 test2 = new class870();
+//        System.out.println(test2.advantageCount(a,b));
 //        class1140 test = new class1140();
 //        System.out.println(test.stoneGameII(a));
 
