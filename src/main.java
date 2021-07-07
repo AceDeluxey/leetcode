f15b3dae56f928c5b09684089ab0daac8b7129b9
PDH <1154621892@qq.com>
2021-07-07 12:44:04 +0800

    凑

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.TreeSet;
 
+import DataStructure.ListNode;
 import  DataStructure.TreeNode;
 public class main {
     public static void main(String[] args) throws ParseException {
@@ -11,18 +12,24 @@ public class main {
         String dateStr = "2019-01-03 10:59:27";
         Date date = simpleDateFormat.parse(dateStr);
         System.out.println(date);
-        int []a={9,10,8,7};
-        int []b={5,6,7,8};
+
+        int []a={1,2,4};
+        ListNode node1 = new ListNode(a);
+
+        int []b={1,3,4};
+        ListNode node2 = new ListNode(b);
         char c='中';
-        System.out.println((int)c);
-        class455 test = new class455();
-        class1510 test1 = new class1510();
-//        System.out.println(test1.winnerSquareGame(15));
-        class1600 test3 = new class1600("king");
-        test3.birth("king","andy");
-        test3.birth("king","bob");
-        test3.birth("andy","s");
-        System.out.println(test3.getInheritanceOrder());
+        class21 test = new class21();
+        test.mergeTwoLists(node1,node2);
+//        System.out.println((int)c);
+//        class455 test = new class455();
+//        class1510 test1 = new class1510();
+////        System.out.println(test1.winnerSquareGame(15));
+//        class1600 test3 = new class1600("king");
+//        test3.birth("king","andy");
+//        test3.birth("king","bob");
+//        test3.birth("andy","s");
+//        System.out.println(test3.getInheritanceOrder());
 //        class870 test2 = new class870();
 //        System.out.println(test2.advantageCount(a,b));
 //        class1140 test = new class1140();
