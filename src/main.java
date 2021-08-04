273512a2c4cfc9f7a985c236d1027a639505f3a3
PDH <1154621892@qq.com>
2021-08-04 22:45:41 +0800

    凑数

--- src/main.java
+++ src/main.java
@@ -9,13 +9,21 @@ import DataStructure.ListNode;
 import  DataStructure.TreeNode;
 public class main {
     public static void main(String[] args) throws ParseException {
-        LinkedList<Integer> stack;
-        stack  = new LinkedList<>();
-        stack.push(1);
-        stack.push(null);
-        stack.push(2);
-        System.out.println(stack);
+//        LinkedList<Integer> stack;
+//        stack  = new LinkedList<>();
+//        stack.push(1);
+//        stack.push(null);
+//        stack.push(2);
+//        System.out.println(stack);
+//[[2,1],[3,4],[3,2]]
 
+//        class37 test = new class37();
+//        char [][]board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+//        test.solveSudoku(board);
+
+
+        class17 test = new class17();
+        test.letterCombinations("");
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 //        String dateStr = "2019-01-03 10:59:27";
 //        Date date = simpleDateFormat.parse(dateStr);
