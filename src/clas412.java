3b6eee3f47a36f9c5f93b9642df33983dac86b79
PDH <1154621892@qq.com>
2021-10-13 09:48:04 +0800

    412. Fizz Buzz 太简单

---
+++ src/clas412.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    412. Fizz Buzz
+    太简单
+    1ms 99%
+ */
+public class clas412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i%3==0 && i%5==0){
+                res.add("FizzBuzz");
+            } else if(i%3==0){
+                res.add("Fizz");
+            } else if(i%5==0){
+                res.add("Buzz");
+            } else{
+                res.add(Integer.toString(i));
+            }
+        }
+        return res;
+    }
+}
