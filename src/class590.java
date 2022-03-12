15b1039799e69ef36fec5679ec83cc72c4e67c92
PDH <1154621892@qq.com>
2022-03-12 11:01:21 +0800

    590. N 叉树的后序遍历

---
+++ src/class590.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 590. N 叉树的后序遍历
+ * 0ms 100%
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+public class class590 {
+//    List<Integer> res;
+//    public List<Integer> postorder(Node root) {
+//        res = new ArrayList<>();
+//        DFS(root);
+//        return res;
+//    }
+//    private void DFS(Node node){
+//        if(node == null){
+//            return;
+//        }
+//        for( Node temp:node.children ){
+//            DFS(temp);
+//        }
+//        res.add(node.val);
+//
+//    }
+}
