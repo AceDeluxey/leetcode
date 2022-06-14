c61afbc93b20b0634582a37122578a635df1bbf3
PDH <1154621892@qq.com>
2022-06-14 10:44:38 +0800

    凑

--- src/swordOffer/main.java
+++ src/swordOffer/main.java
@@ -1,8 +1,22 @@
 package swordOffer;
 
+import DataStructure.TreeNode;
+
 public class main {
     public static void main(String[] args) {
-
+        sword30 test = new sword30();
+        test.push(null);
+        test.push(-2);
+        test.push(0);
+        System.out.println(test.min());
+//        sword40 test = new sword40();
+//        int a[]={9,8,4,6,7,2};
+//        test.mergeSort(a,0,a.length - 1);
+        //        树的构造
+//        Integer [] a={3,9,20,null,null,15,7};
+//        TreeNode root1 =new TreeNode(a);
+//        sword32II test = new sword32II();
+//        System.out.println(test.levelOrder(root1));
 
     }
 }
