cc5a86146f70bd8202845749cbc0eb7670fc204c
PDH <1154621892@qq.com>
2022-07-11 22:51:50 +0800

    剑指 Offer II 056. 二叉搜索树中两个节点之和

---
+++ src/swordOffer/swordII056.java
@@ -0,0 +1,28 @@
+package swordOffer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 剑指 Offer II 056. 二叉搜索树中两个节点之和
+ * dfs + hash 3ms 49.49%
+ */
+public class swordII056 {
+//    Set<Integer> set = new HashSet<>();
+//    public boolean findTarget(TreeNode root, int k) {
+//        if(root == null){
+//            return false;
+//        }
+//        if(findTarget(root.left,k)){
+//            return true;
+//        }
+//        if(findTarget(root.right, k)){
+//            return true;
+//        }
+//        if(set.contains(k-(root.val))){
+//            return true;
+//        }
+//        set.add(root.val);
+//        return false;
+//    }
+}
