8dd6298ad9c342dfa1c6f2cf73b78da9d9b31fd6
PDH <1154621892@qq.com>
2022-07-21 23:11:30 +0800

    凑

--- src/swordOffer/main.java
+++ src/swordOffer/main.java
@@ -4,11 +4,16 @@ import DataStructure.TreeNode;
 
 public class main {
     public static void main(String[] args) {
-        sword30 test = new sword30();
-        test.push(null);
-        test.push(-2);
-        test.push(0);
-        System.out.println(test.min());
+//        sword30 test = new sword30();
+//        test.push(null);
+//        test.push(-2);
+//        test.push(0);
+//        System.out.println(test.min());
+        swordII037 test = new swordII037();
+        int a[ ] = {1,-2,-2,-2};
+        test.asteroidCollision(a);
+
+
 //        sword40 test = new sword40();
 //        int a[]={9,8,4,6,7,2};
 //        test.mergeSort(a,0,a.length - 1);
