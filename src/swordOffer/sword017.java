8f3ad0d05820c74d1070384687c9e4aabefe43d1
PDH <1154621892@qq.com>
2022-08-06 22:42:38 +0800

    cou

---
+++ src/swordOffer/sword017.java
@@ -0,0 +1,23 @@
+package swordOffer;
+
+import java.util.HashMap;
+
+public class sword017 {
+    public String minWindow(String s, String t) {
+//        HashMap<Integer, Integer> tS = new HashMap<>();
+//        HashMap<Integer, Integer> tT = new HashMap<>();
+//        for (int i = 0; i < t.length(); i++) {
+//            int temp = t.charAt(i) - 'A';
+//            tT.put(temp, tT.getOrDefault(temp, 0) + 1);
+//        }
+//        int left = 0;
+//        int right = 0;
+//        while(right<s.length()){
+//            right ++;
+//            if(right<s.length() && tS.containsKey(s.charAt(right-'A'))){
+//
+//            }
+//        }
+        return "s";
+    }
+}
