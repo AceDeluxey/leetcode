bed38e18c10b7cdfbe1e541180a02315b01ccca2
PDH <1154621892@qq.com>
2022-12-13 14:18:07 +0900

    1781. 所有子字符串美丽值之和

---
+++ src/class1832.java
@@ -0,0 +1,16 @@
+/**
+ * 1832. 判断句子是否为全字母句
+ * 位运算
+ * 2ms 59
+ */
+public class class1832 {
+    public boolean checkIfPangram(String sentence) {
+        int mask = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            int temp = sentence.charAt(i)-'a';
+            mask = mask | (1<<temp);
+        }
+        //记得可以用十六进制，8421码写，不用1111这样哈哈哈哈哈
+        return mask==0x3ffffff ;
+    }
+}
