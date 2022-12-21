d259ab603577fd9b7fd139e92d4ecd285e9cb7a9
PDH <1154621892@qq.com>
2022-12-21 22:51:11 +0900

    凑

---
+++ src/Twitter.java
@@ -0,0 +1,38 @@
+/*8
+靠
+本来在刷迪杰斯特拉算法
+不知道为啥就点到这题了
+好复杂 哪天再做
+ */
+class Twitter {
+    //每一行代表每一个用户的关注状况
+//    int [][] matrix;
+//    public Twitter() {
+//        matrix =  new int[502][502];
+//    }
+//
+//    public void postTweet(int userId, int tweetId) {
+//
+//    }
+//
+//    public List<Integer> getNewsFeed(int userId) {
+//
+//    }
+//
+//    public void follow(int followerId, int followeeId) {
+//        matrix[followerId][followeeId] = 1;
+//    }
+//
+//    public void unfollow(int followerId, int followeeId) {
+//        matrix[followerId][followeeId] = 0;
+//    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
