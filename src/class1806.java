7128bc4f77dc78b12d86746ba1c14db27653d758
PDH <1154621892@qq.com>
2023-01-09 14:41:34 +0900

    1806. 还原排列的最少操作步数

---
+++ src/class1806.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * 1806. 还原排列的最少操作步数
+ * On方的模拟，14ms 65%
+ */
+public class class1806 {
+    public int reinitializePermutation(int n) {
+        int[] perm = new int[n];
+        int[] target = new int[n];
+        for (int i = 0; i < n; i++) {
+            perm[i] = i;
+            target[i] = i;
+        }
+        int step = 0;
+        while (true) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                if ((i & 1) != 0) {
+                    arr[i] = perm[n / 2 + (i - 1) / 2];
+                } else {
+                    arr[i] = perm[i / 2];
+                }
+            }
+            perm = arr;
+            step++;
+            if (Arrays.equals(perm, target)) {
+                break;
+            }
+        }
+        return step;
+
+    }
+
+}
