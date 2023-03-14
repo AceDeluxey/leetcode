3dccb70ed6abf4d865261fc0420f95640ed7ae67
AceDeluxey <acedeluxe@foxmail.com>
2023-03-14 12:39:02 +0900

    1605. 给定行和列的和求可行矩阵 贪心

---
+++ src/class1605.java
@@ -0,0 +1,24 @@
+/**
+ * 1605. 给定行和列的和求可行矩阵
+ * 1ms 100
+ */
+public class class1605 {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int n = rowSum.length, m = colSum.length;
+        int[][] matrix = new int[n][m];
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            int v = Math.min(rowSum[i], colSum[j]);
+            matrix[i][j] = v;
+            rowSum[i] -= v;
+            colSum[j] -= v;
+            if (rowSum[i] == 0) {
+                ++i;
+            }
+            if (colSum[j] == 0) {
+                ++j;
+            }
+        }
+        return matrix;
+    }
+}
