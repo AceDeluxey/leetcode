ba63536466c31a710b585e0c05279664d088c039
AceDeluxey <acedeluxe@foxmail.com>
2023-03-21 11:04:56 +0900

    2469. 温度转换

---
+++ src/class2469.java
@@ -0,0 +1,9 @@
+/**
+ * 2469. 温度转换
+ *
+ */
+public class class2469 {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
