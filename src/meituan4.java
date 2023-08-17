0f41334965f6eda5c60d48f0c267541b69b2d07b
AceDeluxey <acedeluxe@foxmail.com>
2023-08-17 11:10:09 +0800

    凑

---
+++ src/meituan4.java
@@ -0,0 +1,27 @@
+//import java.util.Scanner;
+//
+//public class meituan4 {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int  n = in.nextInt();
+//        int m = in.nextInt();
+//        int k = in.nextInt();
+//        int matrix[][] = new int [n][m];
+//        for( int i =0 ; i <k;i++){
+//            int x = in.nextInt();
+//            int y = in .nextInt();
+//            matrix[x][i]=1;
+//
+//        }
+//        int  x1 = in.nextInt();
+//        int y1 = in.nextInt();
+//        int x2 = in.nextInt();
+//        int y2= in.nextInt();
+//
+//        int res = dfs(edges,a,b)+dfs(edges,b,a);
+//        System.out.println(res);
+//    }
+//    public   static  int dfs( int x1 ,int y1,int x2, int y2,){
+//
+//    }
+//}
